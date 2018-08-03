e8d5d1ce95f47d073db16fc90da8871e155e06a5
Iksoo Choi <cliff1215@gmail.com>
2018-08-03 16:59:49 -0700

    01.making initial frame

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,86 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
-
 <script>
+import { base64FromUint8Array } from './libraries/lib.js';
+import JSZip from 'jszip';
+import TickList from './components/TickList.vue';
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'app',
+    components: {
+        'entry-list': TickList,
+    },
+    data () {
+        return {
+            zipObject: null,
+            zipEntries: [],
+            imgSource: '',
+            currentIndex: -1,
+        }
+    },
+    methods: {
+        initData() {
+            this.imgSource = ''; 
+            this.currentIndex = -1;
+            this.zipObject = null;
+            this.zipEntries = [];
+        },
+        isExcludedItem(itemName) {
+            return itemName.includes('__MACOSX') || itemName.includes('/.'); 
+        },
+        loadZipFile(evt) {
+            this.initData();
+
+            let file = evt.target.files[0];
+            JSZip.loadAsync(file).then((zip) => {
+                zip.forEach((relativePath, zipEntry) => {
+                    if (!this.isExcludedItem(zipEntry.name)) {
+                        this.zipEntries.push(zipEntry.name);
+                    }
+                });
+                if (this.zipEntries.length > 0) {
+                    this.zipObject = zip;
+                    this.currentIndex = 0;
+                }
+            })
+        },
+        showImage() {
+            if (this.zipObject === null
+                || this.currentIndex < 0 
+                || this.currentIndex >= this.zipEntries.length) return;
+
+            this.zipObject
+                .file(this.zipEntries[this.currentIndex])
+                .async('uint8array')
+                .then((data) => {
+                    this.imgSource = "data:image/jpg;base64," 
+                        + base64FromUint8Array(data);
+                })
+        },
     }
-  }
 }
 </script>
 
+<template>
+    <div id="app">
+        <h3>Choose the local(s) zip file(s)</h3>
+        <input 
+            type="file" 
+            id="file" 
+            name="file" 
+            v-on:change="loadZipFile" 
+        />
+        <hr />
+        <button 
+            v-on:click="showImage"
+        >
+            Show Image
+        </button>
+        <br />
+        <div class="content">
+            <entry-list v-bind:items="zipEntries"></entry-list>
+            <img v-bind:src="imgSource" />
+        </div>
+    </div>
+</template>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
---
+++ src/components/TickList.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'TickList',
+    props: {
+        items: Array,
+    },
+}    
+</script>
+<template>
+    <div>
+        <ul>
+            <li 
+                v-for="(item, index) in items" 
+                v-bind:key="index"
+            >
+                {{ item }}
+            </li>
+        </ul>
+    </div>
+</template>
+<style scoped>
+
+</style>
